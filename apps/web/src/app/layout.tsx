f6ac7c26a7658a4bf5f0c4e216d959115d4dc24a
JohanTran <johan.tran0210@gmail.com>
2025-09-27 16:12:40 +0200

    chore:Install packages

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../index.css";
 import Providers from "@/components/providers";
-import Header from "@/components/header";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
 	title: "jt-portfolio",
@@ -26,14 +14,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body >
 				<Providers>
-					<div className="grid grid-rows-[auto_1fr] h-svh">
-						<Header />
-						{children}
-					</div>
+					{children}
 				</Providers>
 			</body>
 		</html>
