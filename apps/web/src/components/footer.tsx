638ce42d7e3a26083de9c092711b615929a868ad
JohanTran <johan.tran0210@gmail.com>
2025-10-01 14:45:07 +0200

    fix(layout):Fix preliminary layout of portfolio

---
+++ apps/web/src/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="w-full">
+            <nav className="flex w-full justify-between">
+                <p>Curiosity inspires how I explore code. </p>
+                <p>Passion shapes my ideas into experiences.</p>
+            </nav>
+        </footer>
+    )
+}
