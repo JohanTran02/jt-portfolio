1efb1d5b1dadb888d4abaf575f4686564cab2163
JohanTran <johan.tran0210@gmail.com>
2025-10-06 22:10:39 +0200

    feat(footer):Animate footer text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,18 @@ import Footer from "@/components/footer";
 import RubiksCube from "@/components/RubiksCube";
 
 export default function Home() {
+
 	return (
-		<div className="flex flex-col h-lvh">
-			<div className="flex flex-col justify-center h-full items-center px-5">
+		<div className="relative flex flex-col h-lvh">
+			<div className="flex flex-col justify-center h-full items-center">
 				<div className='text-center'>
 					<p className="text-8xl">Johan Tran</p>
 					<p>Full Stack developer based in Sweden</p>
 				</div>
 				<div className="flex items-center gap-2">
-					<p>Curiosity</p>
 					<div className="size-96" >
 						<RubiksCube />
 					</div>
-					<p>Passion</p>
 				</div>
 			</div>
 			<Footer />
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,34 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 export default function Footer() {
+    useGSAP(() => {
+        const tl = gsap.timeline({ defaults: { ease: "power4.inOut", duration: 2 }, });
+
+        tl.set('.footer-animate', {
+            position: 'fixed',
+            right: '50%',
+            bottom: '50%',
+            yPercent: 50,
+            xPercent: 50,
+        });
+
+        tl.to('.footer-animate', { width: '100%', })
+            .to('.footer-animate', {
+                yPercent: 0,
+                bottom: 0,
+            }, '-=25%')
+            .to('.footer-animate', { width: 'auto' }, '-=30%')
+    })
+
     return (
-        <footer className="w-full">
-            <nav className="flex w-full justify-between">
-                <p>Curiosity inspires how I explore code. </p>
-                <p>Passion shapes my ideas into experiences.</p>
-            </nav>
-        </footer>
+        <>
+            <footer className="footer-animate">
+                <nav className="whitespace-nowrap flex justify-between">
+                    <p>Curiosity inspires how I explore code. </p>
+                    <p>Passion shapes my ideas into experiences.</p>
+                </nav>
+            </footer>
+        </>
     )
 }
