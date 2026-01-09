f3d439aeef5d0168e3dfa3715f8bc1c44fb1dad3
JohanTran <johan.tran0210@gmail.com>
2026-01-09 12:46:26 +0100

    feat(contact):Create design for contact page Fixes #10

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import ContactLinks from "@/components/ContactLinks";
+import ContactTitle from "@/components/ContactTitle";
+
 export default function Page() {
     return (
-        <div>
-            contact
-        </div>
+        <main className="relative flex h-lvh">
+            <div className="flex w-full items-center justify-center">
+                <ContactTitle />
+            </div>
+            <div className="flex flex-col items-center justify-center w-full">
+                <ContactLinks />
+            </div>
+        </main>
     )
 }
